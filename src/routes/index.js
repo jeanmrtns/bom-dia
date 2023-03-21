5b526ef57e97051580364966ab7b24897e7e739e
Jean Martins <jeanmrtns4@gmail.com>
2023-03-21 00:21:32 -0300

    Download image
    Resolves #3

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,9 @@ const routes = express.Router()
 routes.post("/custom-image", async (req, res) => {
   const { imageTheme } = req.body
   const requestImage = new RequestImage()
-  const imageLink = await requestImage.execute(imageTheme)
+  await requestImage.execute(imageTheme)
 
-  return res.json({
-    imageLink
-  })
+  return res.status(200).json({ status: "ok" })
 })
 
 module.exports = routes

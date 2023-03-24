6904e00aa4668db38f42c31d1e361f307fe8bce7
LucasSPaiva <lukas.souza.paiva.9@gmail.com>
2023-03-23 22:01:51 -0300

    add: request phrase

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,25 @@
-const express = require("express")
-const RequestImage = require("../use-cases/request-image")
+const express = require('express')
+const RequestImage = require('../use-cases/request-image')
+const { requestPhrase } = require('../use-cases/request-phrase')
 const routes = express.Router()
 
-routes.post("/custom-image", async (req, res) => {
-  const { imageTheme } = req.body
-  const requestImage = new RequestImage()
-  await requestImage.execute(imageTheme)
+routes.post('/custom-image', async (req, res) => {
+	const { imageTheme } = req.body
+	const requestImage = new RequestImage()
+	await requestImage.execute(imageTheme)
 
-  return res.status(200).json({ status: "ok" })
+	return res.status(200).json({ status: 'ok' })
+})
+
+routes.get('/phrase', async (req, res) => {
+	try {
+		const { term } = req.query
+		const phrase = await requestPhrase(term)
+		res.json(phrase)
+	} catch (err) {
+		console.error(err)
+		res.status(500).json({ message: 'Erro ao processar a solicitação' })
+	}
 })
 
 module.exports = routes

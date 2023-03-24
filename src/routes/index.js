e8a6aec3a7f41534671abd7c37abfca09b8b3ab9
Ulysses Ferreira <ulysses2.0@hotmail.com>
2023-03-23 21:20:35 -0300

    inserindo texto na imagem

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const RequestImage = require("../use-cases/request-image")
+const ManageImage = require("../use-cases/manage-image")
 const routes = express.Router()
 
 routes.post("/custom-image", async (req, res) => {
   const { imageTheme } = req.body
   const requestImage = new RequestImage()
+  const manageImage = new ManageImage()
   const imageLink = await requestImage.execute(imageTheme)
+
+  manageImage.execute()
 
   return res.json({
     imageLink
--- src/use-cases/manage-image.js
+++ src/use-cases/manage-image.js
@@ -1,7 +1,22 @@
 class ManageImage {
-
+    
     execute(){
-        
+        const Jimp = require('jimp');
+
+        Jimp.read("./temp/imagem.jpg")
+        .then((image) => {
+            // Do stuff with the image.
+            image.resize(500, 500) // resize
+            Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then((font) => {
+                image.print(font, 175, 25, "Boa noite");
+                image.print(font, 175, 450, "Boa semana");
+                image.write('./temp/textOverlay.jpg');
+            });
+        })
+        .catch((err) => {
+            // Handle an exception.
+            console.log(err);
+        });
     }
 
 }

0995650a2f1685910f7d5aababd379825fb91308
Ulysses Ferreira <ulysses2.0@hotmail.com>
2023-04-01 20:19:23 -0300

    limitando linhas a 35 caracteres

--- src/use-cases/manage-image.js
+++ src/use-cases/manage-image.js
@@ -2,16 +2,22 @@ const { createCanvas, loadImage } = require("canvas");
 const fs = require("fs");
 
 class ManageImage {
-    //82 char por linha
-    //76 char por linha considerando padding
+    //41 char por linha
+    //35 char por linha considerando padding
+    //13 linhas no maximo
     async execute(phrase){
 
         try {
             loadImage("./tmp/output.png").then((image) => {
-                const palavras = phrase.split(" ")
-                const metade = Math.round((palavras/2))
-
                 
+                // phrase = 'O amor começa quando uma pessoa se sente só e termina quando uma pessoa deseja estar só.'
+                // phrase = 'Se você fosse uma música seria as melhores notas'
+                console.log(phrase);
+                let lines = phrase.match(/.{1,35}(\s|$)/g);
+                let new_phrase = lines.join("\n");
+                let number_lines = lines.length
+                let bottom_padding = 10
+                let y_pos_phrase = (image.height-(45*number_lines)) - bottom_padding
 
                 const canvas = createCanvas(image.width, image.height);
                 const context = canvas.getContext("2d");
@@ -19,16 +25,13 @@ class ManageImage {
                 context.fillStyle = "white";
                 context.fillRect(0, 0, image.width, image.height);
                 context.drawImage(image, 0, 0);
-                context.strokeStyle = "rgb(255, 0, 0)";
-                context.fillStyle = "rgb(212, 66, 245)";
-                // context.font = "70px Arial";
                 context.font = "bold 48px Courier";
-                context.shadowBlur = 10
-                context.textAlign = "center";
-                context.textBaseline = "top"
-                context.fillText("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 0, 0);
-                // context.textBaseline = "bottom"
-                // context.fillText(phrase.split(" ").slice(metade, palavras).join(" "), (image.width/2), (image.height*0.9));
+                context.shadowColor = "rgb(0, 0, 0)";
+                context.shadowBlur = 10;
+                context.strokeStyle = "rgb(0, 0, 0)";
+                context.strokeText(new_phrase, 75-1, y_pos_phrase-1)
+                context.fillStyle = "rgb(212, 66, 245)";
+                context.fillText(new_phrase, 75, y_pos_phrase);
     
                 const buffer = canvas.toBuffer("image/png");
                 fs.writeFileSync("./tmp/saida.png", buffer);

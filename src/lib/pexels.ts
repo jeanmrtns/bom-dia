4f1ff5d6f98bdd2724ccfefc3194b8550cd3cc52
Jean Martins <jeanmrtns4@gmail.com>
2023-04-12 20:58:52 -0300

    refactor: remove pexels api key from code

--- src/lib/pexels.ts
+++ src/lib/pexels.ts
@@ -1,3 +1,3 @@
 import { createClient } from 'pexels'
 
-export const pexels = createClient('mKU6roKLJtleurM1G49hP4M4ofl8OMUvm1zDr5Yx5vYXBPRh1wmtjDoW');
+export const pexels = createClient(String(process.env.PEXELS_API_KEY));
